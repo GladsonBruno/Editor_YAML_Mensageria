7b69df1b3e13018e0b1d3e7f8312f3f0e60e7705
Gladson_Bruno_Arkhi <gladson@arkhi.com.br>
2018-03-08 11:02:14 -0300

    Tela de monificação criada. Tentando escrever no yml

--- main.js
+++ main.js
@@ -1,3 +1,65 @@
 const electron = require('electron');
-const { BrowserWindow, app } = electron;
+const { BrowserWindow, app, dialog, Menu } = electron;
+const url = require('url');
+const path = require('path');
 
+let TelaInicial;
+
+app.on('ready', () => {
+    TelaInicial = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    TelaInicial.loadURL(url.format({
+        pathname: path.join(__dirname, 'config_yml.html'),
+        protocol: 'file:'
+    }));
+
+});
+
+
+let menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Configurar YML',
+                click: () => {
+                    var arquivo = dialog.showOpenDialog(TelaInicial, {
+                        title: 'Selecione o Arquivo YML',
+                        properties: ['openFile'],
+                        callback: (filePaths) => {
+                            return filePaths;
+                        }
+                    });
+                    var contarBarras = 0;
+                    var letraAtual = "";
+                    var caminhoFormatado = "";
+                    for(var i = 0; i < arquivo[0].length; i++){
+                        letraAtual = arquivo.substring(i, i + 1);
+                        if(letraAtual == "\\"){
+                            contarBarras++;
+                            if(contarBarras % 2 != 0){
+                                caminhoFormatado += letraAtual;
+                            }
+                        }
+                    }
+                    console.log(caminhoFormatado);
+                    /*
+                    var telaConfigYML = new BrowserWindow({
+                        width: 800,
+                        height: 600
+                    });
+                    telaConfigYML.loadURL(url.format({
+                        pathname: path.join(__dirname, 'config_yml.html'),
+                        protocol: 'file:'
+                    }));
+                    */                    
+                }
+            }
+        ]
+    }
+]
+
+//const menu = Menu.buildFromTemplate(menuTemplate);
+//Menu.setApplicationMenu(menu);
---
+++ scripts/config-yml.js
@@ -0,0 +1,120 @@
+const yaml = require('js-yaml');
+const yaml_writer = require('write-yaml');
+const fs = require('fs');
+//Importando JQUERY
+const $ = require('jquery');
+
+function teste(){
+    alert('ok');
+}
+
+function salvarInformacoes(){
+    //Arquivo a ser configurado
+    var arquivo_yml = $("#arquivo-yml").val();
+    
+    //Informações Base de Dados
+    var url_db = $("#url-base-de-dados").val();
+    var driver_db = $("#driver-base-de-dados").val();
+    var user_name_db = $("#username").val().replace("'", "");
+    var password_db = $("#password").val().replace("'", "");
+
+    //Informações Certificado 1
+    var codigo_empregador_1 = $("#codigo_empregador_1").val().toString();
+    var path_certificado_1 = $("#caminho-certificado-1").val();
+    var senha_certificado_1 = $("#senha-certificado-1").val();
+    var transmissor_1 = $("#tipo-transmissor-1").val();
+    var numero_transmissor_1 = $("#numero-transmissor-1").val().toString();
+
+    //Informações Certificado 2
+    var codigo_empregador_2 = $("#codigo_empregador_2").val().toString();
+    var path_certificado_2 = $("#caminho-certificado-2").val();
+    var senha_certificado_2 = $("#senha-certificado-2").val();
+    var transmissor_2 = $("#tipo-transmissor-2").val();
+    var numero_transmissor_2 = $("#numero-transmissor-2").val().toString();
+    
+    try {
+        //Iniciando leitura do arquivo. Formato de Resposta = JSON | ARRAY - Não sei ao certo.
+        var doc = yaml.safeLoad(fs.readFileSync(arquivo_yml, 'utf8'));
+    
+        //Alterando informações do arquivo
+
+        //---------------------------------------------------------------------------//
+        //Configurando Base de Dados
+
+        //Url base de dados
+        doc.db.url = url_db;
+        
+        //Driver base de dados
+        doc.db.driver = driver_db;
+
+    //Validation Query
+    /*
+    if(driver_db == "oracle.jdbc.OracleDriver"){
+        doc.db.datasource.validation-query = "SELECT 1 FROM DUAL";
+    } else {
+        doc.db.datasource.validation-query = "SELECT 1";
+    }
+    */
+        //Usuario base de dados
+        doc.db.username = user_name_db;
+
+        //Senha base de dados
+        doc.db.password = password_db;
+
+
+        //---------------------------------------------------------------------------//
+        //Configurando Empregador 1
+
+        //Codigo Empregador 1
+        doc.esocial.empregadores[0].codigo = codigo_empregador_1;
+        
+        //Path Certificado 1
+        doc.esocial.empregadores[0].chave = path_certificado_1;
+
+        //Senha certificado 1
+        doc.esocial.empregadores[0].senha = senha_certificado_1;
+        
+        //Tipo transmissor 1
+        //doc.esocial.empregadores[0].tipo-transmissor = transmissor_1;
+
+        //Numero Transmissor 1
+        //doc.esocial.empregadores[0].numero-transmissor = numero_transmissor_1;
+
+        //---------------------------------------------------------------------------//
+        //Configurando Empregador 2
+
+        //Codigo Empregador 2
+        doc.esocial.empregadores[1].codigo = codigo_empregador_2;
+        
+        //Path Certificado 2
+        doc.esocial.empregadores[1].chave = path_certificado_2;
+
+        //Senha certificado 2
+        doc.esocial.empregadores[1].senha = senha_certificado_2;
+        
+        //Tipo transmissor 2
+        //doc.esocial.empregadores[1].tipo-transmissor = transmissor_2;
+
+        //Numero Transmissor 2
+        //doc.esocial.empregadores[1].numero-transmissor = numero_transmissor_2;
+
+       yaml_writer.sync(arquivo_yml, doc);
+
+        console.log('ok');
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
+
+/*
+try {
+    var doc = yaml.safeLoad(fs.readFileSync('application.yml', 'utf8'));
+    doc.esocial.empregadores[0].codigo = 12345678;
+    doc.esocial.empregadores[1].codigo = 87654321;
+    yaml_writer.sync('application.yml', doc);
+    
+} catch(e) {
+    console.log(e);
+}
+*/
