0a08fd4dd79f135ff1730bafdc309b425f6653d8
GladsonBruno <gladsonbruno16@gmail.com>
2018-04-20 10:24:26 -0300

    Alteração de versão para evitar conflito de build no travis ci

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+//require('./config-yml.js');
+require('../node_modules/materialize-css/dist/css/materialize.min.css');
+require('../css/config-yml.css');
+//require('../node_modules/materialize-css/dist/js/materialize.min.js');
+//require('./jquery.mask.min.js');
+//require('./hammer.min.js');
