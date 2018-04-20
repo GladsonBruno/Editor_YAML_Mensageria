e34c2774a1c1e5f0b94ace59f1d28ceb100a9939
GladsonBruno <gladsonbruno16@gmail.com>
2018-04-20 10:21:43 -0300

    Alteração de versão para evitar conflito de build no travis ci

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+	entry: './scripts/index.js',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.css$/,
+
+				use: [
+					{
+						loader: 'style-loader',
+
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'css-loader'
+					}
+				]
+			}
+		]
+	},
+
+};
